98a5bda11371b7638cea31e5b953494adaeb1017
Tasnuva Rahman <tasnuvarahman1996@gmail.com>
2021-05-01 15:39:03 -0700

    making custom navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../images/logo.png";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCodepen, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons' 
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCodepen,
+  faGithub,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
+  let [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <div className="navbar-main">
-      <div>
-        <div>
-          <NavLink to="/">
-            <img src={logo} alt="" />
-          </NavLink>
+    <div>
+    <div className="bars">
+          <button onClick={() => setIsNavOpen(!isNavOpen)}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
         </div>
+      <div className="navbar-main" style={{display:"none"}}>
+        
         <div>
-          <nav className="nav-main">
-            <ul className="nav-ul work-sans-font">
-              <li className="nav-list">
-                <NavLink className="nav-link" to="/about">About</NavLink>
-              </li>
-              <li className="nav-list">
-                <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
-              </li>
-              <li className="nav-list">
-                <NavLink className="nav-link" to="/blog">Blog</NavLink>
-              </li>
-              <li className="nav-list">
-                <NavLink className="nav-link" to="/contact">Contact</NavLink>
-              </li>
-              <li className="nav-list">
-              <a className="nav-link nav-icon" href="https://github.com/tasnuvatina" target='_blank' rel='norefferer'><FontAwesomeIcon icon={faGithub} /></a>
-                <a className="nav-link nav-icon" href="https://www.linkedin.com/in/tasnuva-rahman/" target='_blank' rel='norefferer'><FontAwesomeIcon icon={faLinkedinIn} /></a>
-                <a className="nav-link nav-icon" href="https://codepen.io/tasnuvarahman" target='_blank' rel='norefferer'><FontAwesomeIcon icon={faCodepen} /></a>
-                
-              </li>
-            </ul>
-          </nav>
+          <div>
+            <NavLink to="/">
+              <img src={logo} alt="" />
+            </NavLink>
+          </div>
+          <div>
+            <nav className="nav-main nav-width">
+              <ul className="nav-ul work-sans-font">
+                <li className="nav-list">
+                  <NavLink className="nav-link" to="/about">
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-list">
+                  <NavLink className="nav-link" to="/portfolio">
+                    Portfolio
+                  </NavLink>
+                </li>
+                <li className="nav-list">
+                  <NavLink className="nav-link" to="/blog">
+                    Blog
+                  </NavLink>
+                </li>
+                <li className="nav-list">
+                  <NavLink className="nav-link" to="/contact">
+                    Contact
+                  </NavLink>
+                </li>
+                <li className="nav-list">
+                  <a
+                    className="nav-link nav-icon"
+                    href="https://github.com/tasnuvatina"
+                    target="_blank"
+                    rel="norefferer"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                  <a
+                    className="nav-link nav-icon"
+                    href="https://www.linkedin.com/in/tasnuva-rahman/"
+                    target="_blank"
+                    rel="norefferer"
+                  >
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                  </a>
+                  <a
+                    className="nav-link nav-icon"
+                    href="https://codepen.io/tasnuvarahman"
+                    target="_blank"
+                    rel="norefferer"
+                  >
+                    <FontAwesomeIcon icon={faCodepen} />
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
