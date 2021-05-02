f3a7fb1dec2d2a73688a95e650b7a627a9a46d03
Tasnuva Rahman <tasnuvarahman1996@gmail.com>
2021-05-02 06:23:24 -0700

    pushing to submit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,14 +18,14 @@ const Home = () => {
                             </div>
                     </div>
                     <div className='welcome'>
-                            <h5 className="poppins-font">Hey there,</h5>
+                            {/* <h5 className="poppins-font">Hey there,</h5>
                             <h1 className="poppins-font">I'm <span className='name-span'>Tasnuva</span>,</h1>
                             <h2 className="work-sans-font">Web Developer & Programmer</h2>
                             <p className="work-sans-font all-p">A self-driven and passionate web developer with a passion for JavaScript.To work in the Software industry with modern web technologies of different local & multinational Software/ IT agencies of Bangladesh and grow rapidly with increasing responsibilities.</p>
                             <div>
                                 <a href={resume} download><button className="work-sans-font">Resume</button></a>
                                 <Link to ='/contact'><button className="work-sans-font">Contact me</button></Link>
-                            </div>
+                            </div> */}
                     </div>
                     <div className='home-animation'></div>
             </div>
